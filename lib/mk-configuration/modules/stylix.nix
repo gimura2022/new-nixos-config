8973c963864ffe8886b1b187090c44bad09d96d7
gimura <gimura0001@gmail.com>
2026-01-16 21:17:23 +0300

    changed stylix theme

--- lib/mk-configuration/modules/stylix.nix
+++ lib/mk-configuration/modules/stylix.nix
@@ -2,6 +2,6 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
   };
 }
